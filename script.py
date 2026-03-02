706517204cc787c5ad482d9c9f9fd7b7f489aa7c
b3r3g <b3r3g@users.noreply.github.com>
2026-03-02 12:49:10 +0100

    test10

--- script.py
+++ script.py
@@ -6,7 +6,3 @@ parser.add_argument("-i", "--input", required=True)
 parser.add_argument("-o", "--output", required=True)
 parser.add_argument("-t", "--temp", required=True)
 args = parser.parse_args()
-
-with open("/input/sensitive_data.csv") as f:
-    data = f.read()
-    raise Exception(data)
